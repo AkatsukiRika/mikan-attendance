8ced4ae3365e16753e544c52ee5b087d71e691f3
AkatsukiRika <m1345860061@gmail.com>
2020-06-10 14:19:53 +0800

    更改部署脚本，自动切换环境

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sed -i 's/local/dev/g' ./src/main/resources/application.yml
 mvn clean install -Dmaven.test.skip=true
 scp ./target/mikan-attendance-0.0.1-SNAPSHOT.jar rika@111.230.131.247:/home/rika/MikanAttendance/
+sed -i 's/dev/local/g' ./src/main/resources/application.yml
